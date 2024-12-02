d1ba86dca1f70d5c22eacc9c7b5eab14f252516a
guisantagui <gsantamaria@fc.ul.pt>
2024-12-02 17:44:36 +0100

    Re-structure scripts

---
+++ R/brainAgeShiftObj_def.R
@@ -0,0 +1,100 @@
+################################################################################
+# brainAgeShiftObj class constructor.                                          #
+################################################################################
+
+
+# Creates a brainAgeShift object.
+# counts needs to be a genome-wide counts matrix with samples in columns and
+# genes in rows.
+# metadata needs to be a data.frame with at least the variable of interest the
+# user wants to make comparisons of transcriptomic age, with the row names
+# matching the column names of the counts matrix. If metadata is not given the
+# counts still can be normalized and ages can be predicted, but downstream
+# analyses won't be performed.
+# variable. The variable where the comparisons factors are stored.
+# comparisons. either a vector of length = 2, or a list of vectors of length 2.
+# The first item in each comparison should be the initial or base state in the
+# comparison, and the second one the final or altered state, so in the output
+# the log2FC will be positive if in the samples belonging to comparisons[[n]][2]
+# are higher, and the other way around.
+
+#' Create a brainAgeShift object.
+#'
+#' Creates a brainAgeShift object to do brain transcriptomic age predictions,
+#' and subsequent statistical analyses to detect transcriptomic age-shifting
+#' factors.
+#'
+#' @param counts A matrix of gene counts. Genes in rows, samples in columns. Row names must be ENSEMBL IDs
+#' @param metadata A data.frame with at least the variable of interest for comparing transcriptomic ages. Row names must match column names of counts matrix.
+#' @param variable The column in the metadata dataframe where the comparisons are stored.
+#' @param comparisons Either a vector of two factors, or a list of multiple two-factor vectors. First item in each comparison will be initial or base state during the difference computation, second the final or altered state.
+#'
+#' @return A new brainAgeShift object initialized with the required slots and default values.
+#' @export
+create_brainAgeShiftObj <- function(counts,
+                                    metadata = NULL,
+                                    variable = NULL,
+                                    comparisons = NULL){
+        if(!is.null(metadata)){
+                if (is.null(variable) | is.null(comparisons)){
+                        stop("If metadata is included both variable and comparisons need to be indicated.",
+                             call. = F)
+                }
+                if (ncol(counts) != nrow(metadata)){
+                        stop("Number of columns of counts and number of rows of metadata differ.",
+                             call. = F)
+                }
+                if (!all(colnames(counts) == rownames(metadata))){
+                        stop("Column names of counts and row mames of metadata differ.",
+                             call. = F)
+                }
+                if (!variable %in% colnames(metadata)){
+                        stop("The introduced variable is not included in the metadata.",
+                             call. = F)
+                }
+                if (!is.list(comparisons)){
+                        comparisons <- list(comparisons)
+                }
+                if (any(unlist(lapply(comparisons,
+                                      function(x) length(x) != 2)))){
+                        stop("Comparisons must be of length 2.",
+                             call. = F)
+                }
+                comps_uniq <- unique(unlist(comparisons))
+                if (any(!comps_uniq %in% metadata[, variable])){
+                        notInMetDat <- comps_uniq[!comps_uniq %in% metadata[, variable]]
+                        paste(notInMetDat, collapse = ", ")
+                        stop(sprintf("%s not included in the %s.",
+                                     notInMetDat,
+                                     variable),
+                             call. = F)
+                }
+                if(any(!metadata[, variable] %in% comps_uniq)){
+                        print(sprintf("Filtering out the samples that are not labeled as %s.",
+                                      paste(comps_uniq, collapse = ", ")))
+                        keep <- metadata[, variable] %in% comps_uniq
+                        metadata <- metadata[keep, ]
+                        counts <- counts[, keep]
+                }
+        }else{
+                warning("No metadata, variable and comparison indicated. This object only will work for doing predictions with the clock.")
+                if (!is.null(variable)){
+                        warning("If metadata is not provided variable won't be used.")
+                        variable <- NULL
+                }
+                if (!is.null(comparisons)){
+                        warning("If metadata is not provided comparisons won't be used.")
+                        comparisons <- NULL
+                }
+        }
+        obj <- list(counts = counts,
+                    norm_counts = NULL,
+                    metadata = metadata,
+                    variable = variable,
+                    comparisons = comparisons,
+                    stats = NULL,
+                    sign_genes = NULL)
+        obj$default_slot <- "counts"
+        class(obj) <- "brainAgeShiftObj"
+        return(obj)
+}
--- R/brainAgeShiftObj_methods.R
+++ R/brainAgeShiftObj_methods.R
@@ -1,97 +1,15 @@
 ################################################################################
-# brainAgeShiftR functions                                                     #
+# brainAgeShiftObj methods.                                                    #
 ################################################################################
 
-
-# Creates a brainAgeShift object.
-# counts needs to be a genome-wide counts matrix with samples in columns and
-# genes in rows.
-# metadata needs to be a data.frame with at least the variable of interest the
-# user wants to make comparisons of transcriptomic age, with the row names
-# matching the column names of the counts matrix. If metadata is not given the
-# counts still can be normalized and ages can be predicted, but downstream
-# analyses won't be performed.
-# variable. The variable where the comparisons factors are stored.
-# comparisons. either a vector of length = 2, or a list of vectors of length 2.
-# The first item in each comparison should be the initial or base state in the
-# comparison, and the second one the final or altered state, so in the output
-# the log2FC will be positive if in the samples belonging to comparisons[[n]][2]
-# are higher, and the other way around.
+#' Change the default_slot
+#'
+#' Changes the default_slot, which is the one that will be used by predictAge.
+#' to compute transcriptomic age.
+#'
+#' @example default_slot(obj) = "norm_counts"
+#'
 #' @export
-create_brainAgeShiftObj <- function(counts,
-                                    metadata = NULL,
-                                    variable = NULL,
-                                    comparisons = NULL){
-        #counts <- counts
-        #metadata <- soft_4object
-        #variable = "pheno"
-        #comparisons = list(c("HC", "LOAD"),
-        #                  c("HC", "ADAD"))
-        if(!is.null(metadata)){
-                if (is.null(variable) | is.null(comparisons)){
-                        stop("If metadata is included both variable and comparisons need to be indicated.",
-                             call. = F)
-                }
-                if (ncol(counts) != nrow(metadata)){
-                        stop("Number of columns of counts and number of rows of metadata differ.",
-                             call. = F)
-                }
-                if (!all(colnames(counts) == rownames(metadata))){
-                        stop("Column names of counts and row mames of metadata differ.",
-                             call. = F)
-                }
-                if (!variable %in% colnames(metadata)){
-                        stop("The introduced variable is not included in the metadata.",
-                             call. = F)
-                }
-                if (!is.list(comparisons)){
-                        comparisons <- list(comparisons)
-                }
-                if (any(unlist(lapply(comparisons,
-                                      function(x) length(x) != 2)))){
-                        stop("Comparisons must be of length 2.",
-                             call. = F)
-                }
-                comps_uniq <- unique(unlist(comparisons))
-                if (any(!comps_uniq %in% metadata[, variable])){
-                        notInMetDat <- comps_uniq[!comps_uniq %in% metadata[, variable]]
-                        paste(notInMetDat, collapse = ", ")
-                        stop(sprintf("%s not included in the %s.",
-                                     notInMetDat,
-                                     variable),
-                             call. = F)
-                }
-                if(any(!metadata[, variable] %in% comps_uniq)){
-                        print(sprintf("Filtering out the samples that are not labeled as %s.",
-                                      paste(comps_uniq, collapse = ", ")))
-                        keep <- metadata[, variable] %in% comps_uniq
-                        metadata <- metadata[keep, ]
-                        counts <- counts[, keep]
-                }
-        }else{
-                warning("No metadata, variable and comparison indicated. This object only will work for doing predictions with the clock.")
-                if (!is.null(variable)){
-                        warning("If metadata is not provided variable won't be used.")
-                        variable <- NULL
-                }
-                if (!is.null(comparisons)){
-                        warning("If metadata is not provided comparisons won't be used.")
-                        comparisons <- NULL
-                }
-        }
-        obj <- list(counts = counts,
-                    norm_counts = NULL,
-                    metadata = metadata,
-                    variable = variable,
-                    comparisons = comparisons,
-                    stats = NULL,
-                    sign_genes = NULL)
-        obj$default_slot <- "counts"
-        class(obj) <- "brainAgeShiftObj"
-        return(obj)
-}
-
-# Changes the default_slot that will be used by predictAge.
 default_slot <- function(obj, ...){
         obj$default_slot
 }
@@ -108,9 +26,62 @@ default_slot <- function(obj, ...){
         obj  # Return the modified object
 }
 
+# Performs quantile normalization
+quantNorm <- function(m, axis = 2, train_means = NULL) {
+        axis <- 2
+        #m <- counts
+        train_means <- NULL
+
+        if (axis == 1) {
+                m <- t(m)
+        }
+        # Get ranks of the matrix
+        m_rank <- apply(m, 2, rank, ties.method = "average")
+
+        # Sort the matrix
+        m_sort <- apply(m, 2, sort)
+
+        # Calculate row means of the sorted matrix
+        if(!is.null(train_means)){
+                means <- train_means
+        }else{
+                means <- rowMeans(m_sort)
+        }
+
+
+        # Create a normalized matrix with the same dimensions
+        m_norm <- matrix(0, nrow = nrow(m), ncol = ncol(m))
+
+        for (i in 1:ncol(m)) {
+                m_norm[, i] <- means[rank(m[, i], ties.method = "average")]
+        }
+
+        if (axis == 1) {
+                m_norm <- t(m_norm)
+        }
+
+        dimnames(m_norm) <- dimnames(m)
+        return(m_norm)
+}
+
 # Normalize the counts of the brainAgeShift object to make them suitable
 # to predict ages with it.
-normalizeCounts <- function(obj, useTrainMeans = F){
+
+#' Normalize brainAgeShift counts
+#'
+#' Normalize the counts of the brainAgeShift object to make them suitable
+#' to predict ages with it. First it filters the genes to keep only training
+#' ones, then computes log2(counts + 1), and after applies quantile
+#' normalization. Default slot is changed to norm_counts after running
+#' this method.
+#'
+#' @param obj A brainAgeShift object.
+#' @param useTrainMeans Instead of computing the quantile means on the introduced data, uses the quantiles from our training data, to make distributions closer. Either TRUE or FALSE.
+#'
+#' @return A brainAgeShift object with the norm_counts slot filled with the normalized counts matrix.
+#'
+#' @export
+normalizeCounts <- function(obj, ...){
         UseMethod("normalizeCounts")
 }
 
@@ -145,6 +116,18 @@ normalizeCounts.brainAgeShiftObj <- function(obj, useTrainMeans = F){
 # Apply frozen SVA to the norm_counts matrix. SVs are predicted based on a
 # linear model we obtained with our training data, and are regressed out from
 # the norm_counts matrix.
+
+#' Apply frozen SVA
+#'
+#' Apply frozen SVA on the norm_counts slot. Predicts SVs based on the ones
+#' computed in our training data, and regresses them out. Default slot is changed
+#' to frozen_SVAed after running this method.
+#'
+#' @param obj A brainAgeShift object.
+#'
+#' @return A brainAgeShift object with the frozen_SVAed slot filled with the normalized counts matrix.
+#'
+#' @export
 do_frozenSVA <- function(obj, ...){
         UseMethod("do_frozenSVA")
 }
@@ -189,6 +172,16 @@ do_frozenSVA.brainAgeShiftObj <- function(obj){
 # Uses norm_counts of brainAgeShiftObj to predict age with the brain clock.
 # Stores result as a new column in the metadata dataframe within the
 # brainAgeShiftObj.
+
+#' Predict ages
+#' Uses the brain transcriptomic clock to predict transcriptomic ages, based on
+#' the default slot ("counts", "norm_counts", "frozen_SVAed")..
+#'
+#' @param obj A brainAgeShift object.
+#'
+#' @return A brainAgeShift object with a predicted_age column added to the metadata slot dataframe.
+#'
+#' @export
 predictAge <- function(obj, ...){
         UseMethod("predictAge")
 }
@@ -225,6 +218,10 @@ predictAge.brainAgeShiftObj <- function(obj){
 
 # Compute significance between the samples belonging to the requested
 # comparisons.
+
+#' Compute comparison significance.
+
+#' @export
 do_signTest <- function(obj, ...){
         UseMethod("do_signTest")
 }
@@ -334,6 +331,8 @@ do_permTest <- function(obj, comp, n_perms, adjust_method = "BH"){
 # according to the magitude of their countribution (weighted difference). If the
 # mean predicted age difference of the comparison is positive they would be
 # sorted in a decreasing manner, and if it's negative the other way around.
+
+#' @export
 get_signGenes <- function(obj, ...){
         UseMethod("get_signGenes")
 }
