90fc52a86d3bca18d5c4d64561586de50f24895c
guisantagui <gsantamaria@fc.ul.pt>
2024-12-03 15:28:51 +0100

    Add example data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Example dataset
+#'
+#' example_counts and example_metadata datasets to illustrate the use of the
+#' package.
+#'
+#' @name example_data
+#' @docType data
+#' @usage data(example_data)
+#' @format example_counts (26189 gene counts x 74 samples). example_metadata
+#' (6 columns, containing GSE, GSM, sample name, treatment, pheono and age).
+#'
+#' @source DOI: 10.1126/science.adl2992
+NULL
